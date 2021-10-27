9d0637d95e104eb8df89d6d8ab4bc008f4cf46b5
Piron Evgeniy <piron.johny@gmail.com>
2021-10-27 13:37:38 +0000

    correct HW # 4

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -2,9 +2,6 @@ const counterBtn = document.querySelectorAll("button");
 const counterValue = document.querySelector("#value");
 counterValue.textContent = 0;
 
-const decrementBtn = counterBtn[0];
-const incrementBtn = counterBtn[1];
-
 const counterBtnClick = () => {
   counterBtn.forEach(btn => {
     
