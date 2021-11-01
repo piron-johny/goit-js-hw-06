9261b41ad8bb7e3e1b7352f2fa14f41e19594946
Piron Evgeniy <piron.johny@gmail.com>
2021-11-01 14:58:27 +0000

    HW #9, #10 start

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,18 @@
+
+
+const refs = {
+  colorBtnEl: document.querySelector('.change-color'),
+  colorHexEl: document.querySelector('.color'),
+  bodyEl: document.querySelector('body'),
+}
+
+
+refs.colorBtnEl.addEventListener('click', onClickBtnColor);
+
+function onClickBtnColor() {
+  refs.bodyEl.style.backgroundColor = (getRandomHexColor());
+  refs.colorHexEl.textContent = (getRandomHexColor())
+}
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,29 @@
+const refs = { 
+  inputCoutEl: document.querySelector('#controls input'),
+  createBtnEl: document.querySelector('[data-create]'), 
+  destroyBtnEl: document.querySelector('[data-destroy]'),
+};
+
+
+refs.inputCoutEl.addEventListener('input', createBoxes);
+
+refs.createBtnEl.addEventListener('click', createElements);
+
+function createElements(event){
+  
+}
+
+function createBoxes(amount) {
+  console.log(amount.currentTarget.value);
+}
+
+
+function destroyBoxes(){
+
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
