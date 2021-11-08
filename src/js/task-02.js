e69631de6eb3a5bba990b8d195eab9250ae7bb1c
Piron Evgeniy <piron.johny@gmail.com>
2021-11-08 14:43:51 +0000

    HW 2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -9,20 +9,39 @@ const ingredients = [
 
 const list = document.querySelector("#ingredients");
 
-const obj = new DocumentFragment();
+// const obj = new DocumentFragment();
+
+// const makeIngedients = (ingredients) => {
+//   ingredients.forEach((ingredient) => {
+
+//     const createListItem = document.createElement("li");
+//     createListItem.classList.add("item");
+//     createListItem.innerHTML = ingredient;
+//     obj.append(createListItem)
+
+//   });
+
+//   return list.append(obj)
+// }
+
 
 const makeIngedients = (ingredients) => {
-  ingredients.forEach((ingredient) => {
+  
+  const createListItem = document.createElement("li");
+  createListItem.classList.add("item");
 
-    const createListItem = document.createElement("li");
-    createListItem.classList.add("item");
-    createListItem.innerHTML = ingredient;
-    obj.append(createListItem)
+  const b = [];
+  ingredients.forEach((elem, ind ) => {
+
+    console.log('elem ->', elem, 'ind ->', ind);
+    createListItem.textContent = elem
+    b.push(createListItem)
+
+    return
 
   });
-  
-  return list.append(obj)
-}
+  console.log(b.join(','));
+  return list.append(...b);
+};
 
 makeIngedients(ingredients);
-
