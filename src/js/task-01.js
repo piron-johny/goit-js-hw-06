247607c1e5f1812b041001ec80a47109e297159d
Piron Evgeniy <piron.johny@gmail.com>
2021-11-09 08:43:33 +0000

    HW 1 and 2

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,17 +1,16 @@
 const refs = {
-  listEl: document.querySelector('#categories'),
-  listItemEl: document.querySelectorAll('.item'),
-  listItemTitleEl: document.querySelectorAll('h2'),
-  listAnimalsEl: document.querySelectorAll('.item ul'),
-}
-
+  listEl: document.querySelector("#categories"),
+  listItemEl: document.querySelectorAll(".item"),
+  listItemTitleEl: document.querySelectorAll("h2"),
+  listAnimalsEl: document.querySelectorAll(".item ul"),
+};
 
 const listCategories = () => {
   console.log(`Number of categories: ${refs.listEl.children.length}`);
 
-  for (let i = 0; i < refs.listEl.children.length; i += 1) {
+  refs.listItemEl.forEach((el, i) => {
     console.log(`Category: ${refs.listItemTitleEl[i].textContent}`);
-    console.log(`Elements: ${refs.listAnimalsEl[i].children.length}`);
-  }
+    console.log(`Category: ${refs.listAnimalsEl[i].children.length}`);
+  });
 };
-listCategories()
+listCategories();
--- src/js/task-02.js
+++ src/js/task-02.js
@@ -9,39 +9,16 @@ const ingredients = [
 
 const list = document.querySelector("#ingredients");
 
-// const obj = new DocumentFragment();
-
-// const makeIngedients = (ingredients) => {
-//   ingredients.forEach((ingredient) => {
-
-//     const createListItem = document.createElement("li");
-//     createListItem.classList.add("item");
-//     createListItem.innerHTML = ingredient;
-//     obj.append(createListItem)
-
-//   });
-
-//   return list.append(obj)
-// }
-
-
 const makeIngedients = (ingredients) => {
-  
-  const createListItem = document.createElement("li");
-  createListItem.classList.add("item");
+  list.append(
+    ...ingredients.map((elem) => {
+      const createListItem = document.createElement("li");
+      createListItem.classList.add("item");
+      createListItem.textContent = elem;
 
-  const b = [];
-  ingredients.forEach((elem, ind ) => {
-
-    console.log('elem ->', elem, 'ind ->', ind);
-    createListItem.textContent = elem
-    b.push(createListItem)
-
-    return
-
-  });
-  console.log(b.join);
-  return list.append(...b);
+      return createListItem;
+    })
+  );
 };
 
 makeIngedients(ingredients);
